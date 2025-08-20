ed4e2ad4970b98d91a40c0654a2cd39e4091d828
debiasio001 <ingriddebiasio2020@gmail.com>
2025-08-19 21:44:58 -0300

    Aula14

--- Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao08/aula13/BlackJackPlayer.java
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao08/aula13/BlackJackPlayer.java
@@ -46,4 +46,12 @@ public class BlackJackPlayer {
 	public ArrayList<Carta> getCartas() {
 		return cartas;
 	}
+	public int getTotalCarta() {
+		int tot = 0;
+		for (Carta ct : cartas) {
+			tot += ct.getValor();
+			
+		}
+		return tot;
+	}
 }
