920c54d5b5c6088c2263d015ede91fcd138e437a
debiasio001 <ingriddebiasio2020@gmail.com>
2025-09-08 21:42:23 -0300

    Aula25

--- Eclipse-Workspace/projeto_javafx/src/br/com/senaisp/bauru/JavaFXExemplo03.java
+++ Eclipse-Workspace/projeto_javafx/src/br/com/senaisp/bauru/JavaFXExemplo03.java
@@ -5,6 +5,10 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
@@ -13,13 +17,31 @@ public class JavaFXExemplo03 extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Group noRoot = new Group();
+		LinearGradient lg = new LinearGradient(
+				//startX, satarY,endX,endY,proporcional,
+				//forma ciclos gradientes,stops
+				0, 1, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[] {
+						new Stop(0.5,Color.YELLOW),
+						new Stop(1, Color.BLUE)
+				});
+		RadialGradient rg =  new RadialGradient(
+				//angulo focu, dist foco, centroX, cenroY,raio,proporcional
+				//forma ciclos gradientes,stops
+				0, 0, 0.5, 0.5, 1, true, CycleMethod.NO_CYCLE,
+				new Stop[] {
+						new Stop(0, Color.YELLOW),
+						new Stop(0.25, Color.RED),
+						new Stop(0.5, Color.TRANSPARENT)
+				});
 		//criar o triangulo
 		Polygon triangulo = new Polygon(new double[] {
 				210.0,10.0,
 				210.0,210.0,
 				10.0,210.0
 		});
-		triangulo.setFill(Color.rgb(0,0,255,0.5));
+		//triangulo.setFill(Color.rgb(0,0,255,0.5));
+		//triangulo.setFill(lg);
+		triangulo.setFill(rg);
 		triangulo.setLayoutX(50);
 		triangulo.setLayoutY(50);
 		//aicionando o triangulo ao group
